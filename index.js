a9a5655d29bb11d23678346021384274b056beba
venkanna babu guthula <g.venkanna37@gmail.com>
2018-11-01 01:06:41 +0530

    updated index file

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
+// Dijkstra's Algorithm
+
+// Reading geojson file
+
 var fs = require('fs');
 var data = fs.readFileSync('./geojson/data1.geojson');
 data = JSON.parse(data);
-console.log(data);
+
+// Array of Segments
+
+var segments = [];
+for (var i = 0; i < data.features.length; i++) {
+segments.push(data.features[i].geometry.coordinates);
+}
+
+// Array of coordinates
+
+var seg_coords = [];
+for (var i = 0; i < data.features.length; i++) {
+  for (var j = 0; j < data.features[i].geometry.coordinates.length; j++) {
+      if (data.features[i].geometry.coordinates[j]) {
+        seg_coords.push(data.features[i].geometry.coordinates[j]);
+      }
+  }
+}
+//console.log(seg_coords);
+
+var coords = [];
+for (var i = 0; i < seg_coords.length; i++) {
+  for (var j = 0; j < coords.length; j++) {
+    if (coords[j]!=seg_coords[i]) {
+      coords.push(seg_coords[i]);
+    }
+  }
+}
+console.log(coords);
